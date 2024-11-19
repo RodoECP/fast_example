01964bf88dfba3a1aad52872409271f6b4643173
RodoECP <rodolfoecope@gmail.com>
2024-11-19 10:26:44 -0600

    Primer add

---
+++ api.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Bienvenidos"
+
+@app.get("/name/{name}")
+def greeting(name:str):
+    return "Hello " + name
